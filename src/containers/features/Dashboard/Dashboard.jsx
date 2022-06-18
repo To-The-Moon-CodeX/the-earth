1809794d4943acb75aad9cce67ff117a7d536193
kurosh.husseini <Kurosh.Husseini@metropolia.fi>
2022-06-18 15:31:34 +0300

    fixes

--- src/containers/features/Dashboard/Dashboard.jsx
+++ src/containers/features/Dashboard/Dashboard.jsx
@@ -5,12 +5,21 @@ import styles from "./Dashboard.module.css";
 export const Dashboard = () => {
   const router = useRouter();
   return (
-    <p className={styles.test}>testssssssssssssssssssssssssssssssssss</p>
-    /*<section id="dashboard">
-      <div className="container dashboard__container">
-
-      </div>
-    </section>
-    */
-  )
+    <div>
+      <p className={styles.test}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime laborum
+        officia, repellat possimus ipsum magni tempore totam molestiae
+        blanditiis voluptate qui ratione sapiente, laudantium amet eaque atque
+        vitae adipisci ducimus?
+      </p>
+    </div>
+  );
 };
+
+{
+  /* /*
+<section id="dashboard">
+  <div className="container dashboard__container"></div>
+</section>
+*/
+}
