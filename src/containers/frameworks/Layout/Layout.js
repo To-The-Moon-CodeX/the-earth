e64b5932a324a68c1e75f17a0327693336e7673e
kurosh.husseini <Kurosh.Husseini@metropolia.fi>
2022-06-18 11:06:03 +0300

    Minor fix in layout

--- src/containers/frameworks/Layout/Layout.js
+++ src/containers/frameworks/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigation } from "../Navigation";
-import styles from "./Layout.module.css";
+
 export const Layout = (props) => {
   return (
     <React.Fragment>
