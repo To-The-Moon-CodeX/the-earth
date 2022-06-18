b8187506b00cba2bb818215dc89569a6654a4567
kurosh.husseini <Kurosh.Husseini@metropolia.fi>
2022-06-18 23:52:29 +0300

    Added styling for address info

--- src/containers/features/Dashboard/Dashboard.jsx
+++ src/containers/features/Dashboard/Dashboard.jsx
@@ -2,29 +2,29 @@ import { useRouter } from "next/router";
 import React from "react";
 import Image from "next/image";
 import styles from "./Dashboard.module.css";
+import { AddressInfo } from "./AddressInfo";
 
 export const Dashboard = () => {
   const router = useRouter();
   return (
     <section className={styles.dashboardC}>
-      <div className={styles.dashboardC}>
+      <div className={styles.dashboardCardItem}>
+        <AddressInfo />
+      </div>
+      <div className={styles.dashboardCardItem}>
         <p>tests</p>
       </div>
-      <div className={styles.dashboardC}>
+      <div className={styles.dashboardCardItem}>
         <p>tests</p>
       </div>
-      <div className={styles.dashboardC}>
-        <p>tests</p>
-      </div>
-      <div className={styles.dashboardC}>
+      <div className={styles.dashboardCardItem}>
         <p>tests</p>
       </div>
     </section>
-  )
+  );
 };
 /*
 import React from "react";
-import { AddressInfo } from "./AddressInfo";
 import { AddressTransactions } from "./AddressTransactions";
 import {SendEthereum} from "./SendEthereum"
 
