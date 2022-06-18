60951c728c6346637009e19cf538a304c312dc8a
Santeri Pohjaranta <santeri.pohjaranta@outlook.com>
2022-06-18 16:57:10 +0300

    File for connection to mongodb

---
+++ src/containers/features/SignIn/sign_in_function.js
@@ -0,0 +1,15 @@
+import { connectToDatabase } from "service/mongodb/util/mongodb";
+
+export default async (req, res) => {
+  const { db } = await connectToDatabase();
+  var nickname = "Santeri";
+  switch (req.method){
+    case 'GET':{
+        const movies = await db
+        .collection("login")
+        .find({Username: nickname})
+        .toArray();
+        res.json(movies);
+    }
+  }
+};
