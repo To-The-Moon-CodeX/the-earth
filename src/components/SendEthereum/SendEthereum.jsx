24f742ea28f2b8370cbeda5e6e4bcdbff845ca2a
estsvq <ezasdi@gmail.com>
2022-06-18 19:49:57 +0300

    small clean of code and commenting

--- src/components/SendEthereum/SendEthereum.jsx
+++ src/components/SendEthereum/SendEthereum.jsx
@@ -3,57 +3,35 @@ import React, { useEffect, useState } from "react";
 
 
 export const SendEthereum = () => {
-    
+  /**
+   *
+   * Send ethereum to our bot account.
+   *
+   * @param {string} _quantity Quantity in usd to convert to ethereum
+   */
 
-   /**
-    * 
-    * Quantity in usd to convert to ethereum
-    * @param {string} _quantity 
-    */
-
-   async function getEthPrice() {        
-        fetch("/api/getcryptoprice")
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            return data.ethereum.usd;
-        })
-        .catch((err) => console.error(err));
-        
-   }
   async function sendButton(_quantity) {
-
-
-
     try {
       let ethereum = window.ethereum;
 
       if (ethereum) {
-
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
 
         const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
+          method: "eth_requestAccounts",
         });
 
-        
-        //const usdToEthDone = await getEthPrice().then((res) => {console.log(res); return res});
-         
-        // const quantity = "";
-
         const usdToEthDone = await fetch("/api/getcryptoprice")
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);            
-            let dividedQuantity = (_quantity/data.ethereum.usd);
-            quantity = ethers.utils.parseEther(dividedQuantity.toString());
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            let dividedQuantity = _quantity / data.ethereum.usd;
             return dividedQuantity;
-        })
-        .catch((err) => console.error(err));
+          })
+          .catch((err) => console.error(err));
 
         console.log(usdToEthDone.toString());
-
 
         const quantity = ethers.utils.parseEther(usdToEthDone.toString());
 
@@ -65,14 +43,13 @@ export const SendEthereum = () => {
           .catch((error) => console.error(error));
 
         console.log("tx:", tx);
-
       } else {
         console.log("Ethereum object doesn't exist!");
       }
     } catch (error) {
       console.log(error);
     }
-  };
+  }
 
   return (
     <div>
